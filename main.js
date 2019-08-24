d0e6c711c662904d6954742e73996a7d0ab03c24
paulo-fontes <paulo.sist@gmail.com>
2019-08-24 00:25:07 +0000

    d

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function chamarAPI(){
 	console.log("caiu aqui");
-	const endpoint = "api/latest";
+	const endpoint = "https://api.exchangeratesapi.io/latest";
 	var oParametros = {
 		base: $("#moeda_origem").val()
 	};
