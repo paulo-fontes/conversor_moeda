f62db135e4422b1811bc589d97fb43932dab9721
paulo-fontes <paulo.sist@gmail.com>
2019-08-24 01:25:31 +0000

    remoção das funções globais

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-function chamarAPI(){
-	console.log("caiu aqui");
-	const endpoint = "api/latest"; 
-	var oParametros = {
-		base: $("#moeda_origem").val()
-	};
-	$.get(endpoint, oParametros, callback );
-}
+const ovly = { };
 
-function callback(resultado){
-	console.log(resultado)
-	var moeda_destino = $("#moeda_destino").val();
-	var fValor = resultado.rates[moeda_destino];
-	$("#valor_destino_cel").text(fValor);
-	$("#moeda_destino_cel").text( moeda_destino);
+ovly.conversor = {
+	versao: "1",
+	chamarAPI: function(){
+			console.log("caiu aqui");
+			const endpoint = "api/latest"; 
+			var oParametros = {
+				base: $("#moeda_origem").val()
+			};
+		$.get(endpoint, oParametros, this.callback );
+	},
+	callback: function(resultado){
+			var moeda_destino = $("#moeda_destino").val();
+			var fValor = resultado.rates[moeda_destino];
+			$("#valor_destino_cel").text(fValor);
+			$("#moeda_destino_cel").text( moeda_destino);
+	}
 }
